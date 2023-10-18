90a813d7b15bf8a178f61d5e9b97211f8180fb97
Quazi Marufur Rahman <maruf.csdu@gmail.com>
2023-10-18 18:57:34 +1000

    config updated

--- config.py
+++ config.py
@@ -1,12 +1,6 @@
-from pathlib import Path
-
-
 class Config:
     chunk_size = 1000
     chunk_overlap = 100
     vectorstore_dir = 'docs'
     vectorstore_path = f'{vectorstore_dir}/vectorstore.pkl'
     chatgpt_model_name = 'gpt-3.5-turbo'
-
-
-Path(Config.vectorstore_dir).unlink(missing_ok=True)
